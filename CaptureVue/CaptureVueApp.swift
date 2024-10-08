c27c34f415fcbc5f9a02b11545996c83c545e583
Panagiotis Makris <panosmakris7@windowslive.com>
2024-10-08 22:26:47 +0300

    first commit

---
+++ CaptureVue/CaptureVueApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureVueApp.swift
+//  CaptureVue
+//
+//  Created by Paris Makris on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureVueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureVue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptureVue
+//
+//  Created by Paris Makris on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
